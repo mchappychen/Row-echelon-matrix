fbddc430a3ac7a4b2d3639a1d321f68136d88c61
Michael <51720724+mchappychen@users.noreply.github.com>
2019-06-19 10:30:53 -0400

    Added user manual input, rowEchelon() not working

--- rowEchelon.py
+++ rowEchelon.py
@@ -35,6 +35,7 @@ def rowEchelon(a):
     i = 0 #1
     j = 0
     while(j < len(a[0])):
+        operate = True
         if(a[i][j] == 0): #2 If it's 0, switch it with a row below where pivot won't become 0
             switched = False
             for x in range(i+1,len(a)):
@@ -46,16 +47,94 @@ def rowEchelon(a):
                     break
             if(not switched):
                 j += 1
-        else: #3 Multiply row i by 1/pivot to make pivot = 1
+                operate = False
+        if(operate):
+            #3 Multiply row i by 1/pivot to make pivot = 1
             a[i] = multiply(1.0/a[i][j] , a[i])
             #4 Turn every element below the pivot (in column j) into 0
-            
+            for x in range (i+1,len(a)):
+                if(a[x][j] != 0):
+                    a[x] = add(multiply(-1 * a[x][j],a[i]),a[x])
+            i += 1 #5
+            j += 1
+    return a
+
+def checkMatrix(a): #checks if a[] is in correct format
+    for x in range(len(a)):
+        for y in range(len(x)):
+            try:
+                if(a[x][y] == None):
+                    print("Error in checkMatrix(): (",x,",",y,") is empty")
+                    return False
+            except IndexError:
+                print("Error in checkMatrix(): (",x,",",y,") is missing, matrix is not rectangle")
+                return False
+    return True
+    
 def main():
     """Plan:
         1. Give user option to input matrix manually, automatically, or exit
-        2. Matrix inputted should be rounded to 2 decimals
+        2. Give user option to specifiy rows and columns (m,n)
+        3. Call rowEchelon(a)
     """
-    print("Hi idiot")
+    m = 0 #rows
+    n = 0 #columns
+    goodInput = False
+    userInput = ""
+    while(not goodInput): #1 Give user option to input matrix manually, automatically, or exit
+        userInput = input("Type '1' for manual input, '2' for auto-input, '3' to exit :: ")
+        if(userInput == '1'):
+            goodInput = True
+            userInput = "manual"
+        elif(userInput == '2'):
+            goodInput = True
+            userInput = "auto"
+        elif(userInput == '3'):
+            goodInput = True
+            return None
+        else:
+            print("Only enter 1, 2 or 3 you idiot, learn to read.")
+    goodInput = False
+    while(not goodInput): #2 Give user option to specify rows and columns (m,n)
+        try:
+            m = int(input("Enter rows :: "))
+            if(not m>0):
+                m = int("beef")
+            goodInput = True
+        except ValueError:
+            print("Type in a positive natural number idiot, use your head")
+    goodInput = False
+    while(not goodInput):
+        try:
+            n = int(input("Enter columns :: "))
+            if(not n>0):
+                n = int("beef")
+            goodInput = True
+        except ValueError:
+            print("Type in a positive natural number idiot, use your head")
+    if(userInput == "manual"):
+        matrix = []
+        for x in range(m):
+            row = []
+            for y in range(n):
+                goodInput = False
+                while(not goodInput):
+                    try:
+                        row.append(float(input("Enter number :: ")))
+                        goodInput = True
+                    except ValueError:
+                        print("Type in a number idiot, learn to read") 
+            matrix.append(row)
+        checkMatrix(matrix)
+        print("\nMatrix looks like:")
+        printMatrix(matrix)
+        print("Row-Echelon matrix looks like:")
+        printMatrix(rowEchelon(matrix)) #3 Call rowEchelon()
+    elif(userInput == "auto"):
+        pass
+    else:
+        print("Error in main(): This line should not have been executed")
+        return None
     
 if __name__ == "__main__":
     main()
